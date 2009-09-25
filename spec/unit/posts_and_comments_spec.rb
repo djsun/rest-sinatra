0a2867cea8f76024bb9ec7c74b8b89e51fac809e
David James <djames@sunlightfoundation.com>
2009-09-25 10:22:53 -0400

    Rearranging spec files

--- spec/unit/posts_and_comments_spec.rb
+++ spec/unit/posts_and_comments_spec.rb
@@ -1,6 +1,6 @@
-require File.expand_path(File.dirname(__FILE__) + '/../helpers/spec_helper.rb')
-require File.expand_path(File.dirname(__FILE__) + '/../helpers/posts.rb')
-require File.expand_path(File.dirname(__FILE__) + '/../helpers/comments.rb')
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper.rb')
+require File.expand_path(File.dirname(__FILE__) + '/helpers/posts.rb')
+require File.expand_path(File.dirname(__FILE__) + '/helpers/comments.rb')
 
 describe "Posts" do
   
--- spec/unit/sources_spec.rb
+++ spec/unit/sources_spec.rb
@@ -1,5 +1,5 @@
-require File.expand_path(File.dirname(__FILE__) + '/../helpers/spec_helper')
-require File.expand_path(File.dirname(__FILE__) + '/../helpers/sources')
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/helpers/sources')
 
 describe "Sources" do
   
